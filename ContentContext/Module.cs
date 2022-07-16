6353c5f2e179aaec103f4fa680d759d77c6e2ea1
Diego <diego.p.campos@animaeducacao.com.br>
2022-07-16 11:52:03 -0300

    Método construtor

--- ContentContext/Module.cs
+++ ContentContext/Module.cs
@@ -4,7 +4,7 @@ namespace Balta.ContentContext
   {
     public Module()
     {
-      Lectures = new List<Lecture>();
+      var Lectures = new List<Lecture>();
     }
     public int Order { get; set; }
     public string Title { get; set; }
--- Program.cs
+++ Program.cs
@@ -7,7 +7,17 @@ namespace Balta
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var articles = new List<Article>();
+            articles.Add(new Article("Artigo sobre OOP", "orientacao-objetos"));
+            articles.Add(new Article("Artigo sobre C#", "orientacao-csharp"));
+            articles.Add(new Article("Artigo sobre dotnet", "orientacao-dotnet"));
+
+            foreach (var article in articles)
+            {
+                Console.WriteLine(article.Id);
+                Console.WriteLine(article.Title);
+                Console.WriteLine(article.Url);
+            }
         }
     }
 }
