deb8497aca5a361291708a62c2c17dd91ba6808a
Diego <diego.p.campos@animaeducacao.com.br>
2022-07-16 12:22:54 -0300

    Notificações e Herança

--- ContentContext/Article.cs
+++ ContentContext/Article.cs
@@ -4,7 +4,6 @@ namespace Balta.ContentContext
 {
   public class Article : Content
   {
-    public IList<Notification> Notifications { get; set; }
     public Article(string title, string url) : base(title, url)
     {
     }
--- ContentContext/Module.cs
+++ ContentContext/Module.cs
@@ -1,14 +1,13 @@
 namespace Balta.ContentContext 
 {
- public class Module
+ public class Module : Base
   {
     public Module()
     {
-      var Lectures = new List<Lecture>();
+       Lectures = new List<Lecture>();
     }
     public int Order { get; set; }
     public string Title { get; set; }
-
     public IList<Lecture> Lectures { get; set; }
   }
 
